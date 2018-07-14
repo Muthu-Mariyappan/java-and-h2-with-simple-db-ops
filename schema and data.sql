82cba66a7b66ff22b7b37244473e364d6e9e2947
Muthu Mariyappan <ggmm.muthu@gmail.com>
2018-07-14 09:46:37 +0530

    commit

--- schema and data.sql
+++ schema and data.sql
@@ -9,10 +9,10 @@ insert into department values(101,'IT') ;
 insert into department values(102,'ECE') ;
 
 
-insert into student(name,cgpa,deptid) values(1,'Harry',9.20,100) ;
-insert into student(name,cgpa,deptid) values(2,'Percy',8.50,100) ;
-insert into student(name,cgpa,deptid) values(3,'Ronnie',7.20,101) ;
-insert into student(name,cgpa,deptid) values(4,'Lisa',6.66,102) ;
-insert into student(name,cgpa,deptid) values(5,'Albus',9.46,101) ;
-insert into student(name,cgpa,deptid) values(6,'Grover',8.12,102) ;
-insert into student(name,cgpa,deptid) values(7,'Annabeth',9.54,100) ;
+insert into student values(1,'Harry',9.20,100) ;
+insert into student values(2,'Percy',8.50,100) ;
+insert into student values(3,'Ronnie',7.20,101) ;
+insert into student values(4,'Lisa',6.66,102) ;
+insert into student values(5,'Albus',9.46,101) ;
+insert into student values(6,'Grover',8.12,102) ;
+insert into student values(7,'Annabeth',9.54,100) ;
